bf2e222aada82dea2bbcfdc8b07107a5f2f9effd
Christer Edwards <christer.edwards@ubuntu.com>
2010-02-06 15:03:31 -0700

    functional password update function

--- handler.php
+++ handler.php
@@ -13,27 +13,68 @@ foreach ($_GET as $key => $value) {
     $_GET[$key] = mysql_real_escape_string($value);
 }
 
-if (!empty($_POST['login'])) {
+function disturbed_login() {
 
-    if (!empty($_POST['username']) && !empty($_POST['password'])) {
+    if (!empty($_POST['login'])) {
     
-        $validation = mysql_query("SELECT * FROM accounts WHERE username='{$_POST['username']}' AND password='".md5($_POST['password'])."'")
-            or die(mysql_error());
+        if (!empty($_POST['username']) && !empty($_POST['password'])) {
         
-        $rowcount = mysql_num_rows($validation);
+            $validation = mysql_query("SELECT * FROM accounts WHERE username='{$_POST['username']}' AND password='".md5($_POST['password'])."'")
+                or die(mysql_error());
             
-            if ($rowcount == 1) {
-                $_SESSION['username'] = $_POST['username'];
-                header("Location: index.php");
-            } else {
-                header("Location: login.php");
-            }
-
+            $rowcount = mysql_num_rows($validation);
+                
+                if ($rowcount == 1) {
+                    $_SESSION['username'] = $_POST['username'];
+                    header("Location: index.php");
+                } else {
+                    header("Location: login.php");
+                }
+    
+        } else {
+            header("Location: login.php");
+        }
+    
     } else {
         header("Location: login.php");
     }
-
-} else {
-    header("Location: login.php");
 }
+
+function disturbed_change_password() {
+    if (!empty($_POST['update-password'])) {
+    
+        if (!empty($_POST['current-password'])) {
+    
+            $validation = mysql_query("SELECT * FROM accounts WHERE username='{$_SESSION['username']}' AND password='".md5($_POST['current-password'])."'")
+                or die(mysql_error());
+            
+            $rowcount = mysql_num_rows($validation);
+    
+                if ($rowcount == 1) { 
+                    if ($_POST['new-password1'] === $_POST['new-password2']) {
+                        // update new password
+                        mysql_query("UPDATE accounts SET password='".md5($_POST['new-password1'])."' WHERE username='{$_SESSION['username']}'")
+                            or die(mysql_error());
+                        
+                        header("Location: index.php");
+                    } else {
+                        // passwords don't match
+                    }
+                }
+        } else {
+            // current password required
+            print "Please provide your current password.";
+            print "<br />";
+        }
+    }
+}
+
+if (!empty($_POST['login'])) {
+    disturbed_login();
+}
+
+if (!empty($_POST['update-password'])) {
+    disturbed_change_password();
+}
+
 ?>
