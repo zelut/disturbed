1b3108fe0e18c9a7ed7f550710d21de876cc0f07
Christer Edwards <christer.edwards@ubuntu.com>
2010-02-06 09:27:49 -0700

    basic change password form layout created

--- admin.php
+++ admin.php
@@ -1,11 +1,28 @@
 <?php
 
-if ($_SESSION['username'] != 'admin') {
+if ($_SESSION['username'] !== "admin") {
     header("Location: index.php");
 }
 
 require_once("header.inc.php");
+?>
 
+<center>
+    <form method="post" action="handler.php">
+        <br />
+        <b>Password Update:</b><br />
+            Current Password: <br />
+                <input type="password" name="current-password" size="20" tabindex="1" /><br />
+            New Password: <br />
+                <input type="password" name="new-password1" size="20" tabindex="2" /><br />
+            New Password: <br />
+                <input type="password" name="new-password2" size="20" tabindex="3" /><br />
+            <br />
+       <input type="submit" name="update-password" value="submit" tabindex="4" /><br />
+    </form>
+</center>
+
+
+<?php
 require_once("footer.inc.php");
-
 ?>
