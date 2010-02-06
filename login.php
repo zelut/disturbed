104099b961e84abe1e49f73be5631f7ccc2bee84
Christer Edwards <christer.edwards@ubuntu.com>
2010-02-05 23:44:58 -0700

    whitespace cleanup & formatting standardization

--- login.php
+++ login.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
@@ -9,12 +13,10 @@
 <body>
 
     <div id="header">
-
         <h1 class="right">Disturbed</h1>
         <h1><a href="index.php">Disturbed</a></h1>
-
     </div>
-
+    
     <!-- NAVIGATION BEGIN -->
     <ul id="nav">
         <li><a href="index.php">Home</a></li>
@@ -23,25 +25,21 @@
         <li><a href="logout.php">Logout</a></li>
     </ul>
     <!-- NAVIGATION END -->
-
+    
     <div class="clear" />
-
+    
     </div>
 
-<?php
-session_start();
-?>
-
 <center>
-    <form method='POST' action='handler.php'>
+    <form method="post" action="handler.php">
         <br />
         <b>Please Login:</b><br />
             Username: <br />
-                <input type="text" name="username" tabindex="1" /><br />
+                <input type="text" name="username" size="20" tabindex="1" /><br />
             Password: <br />
-                <input type="password" name="password" tabindex="2" /><br />
+                <input type="password" name="password" size="20" tabindex="2" /><br />
             <br />
-       <input type="Submit" name="login" value="Submit" tabindex="3" /><br />
+       <input type="submit" name="login" value="submit" tabindex="3" /><br />
     </form>
 </center>
 
